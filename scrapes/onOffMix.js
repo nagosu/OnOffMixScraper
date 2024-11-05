95971c223c747960b85701b6673259169b4aaece
nagosu <nozick1021@gmail.com>
2024-11-05 11:10:44 +0900

    OnOffMix 크롤링 함수 분리

---
+++ scrapes/onOffMix.js
@@ -0,0 +1,82 @@
+const puppeteer = require('puppeteer-core');
+const chromium = require('@sparticuz/chromium');
+
+async function onOffMixScrape(lastPostTitles) {
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath(),
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  });
+
+  const page = await browser.newPage(); // 새 페이지 생성
+
+  await page.goto('https://onoffmix.com/event/main/?c=105'); // 페이지 URL
+
+  // 로딩 대기
+  await page.waitForSelector('fieldset.filter_category_area');
+
+  // 주제 드롭다운 열기
+  await page.click('fieldset.filter_category_area');
+
+  // 과학/IT/AI 분야 선택
+  await page.evaluate(() => {
+    document
+      .querySelector('fieldset.filter_category_area li:nth-of-type(3) input')
+      .click();
+  });
+
+  // 적용 버튼 클릭
+  await page.evaluate(() => {
+    document
+      .querySelector('fieldset.filter_category_area div.button_area button')
+      .click();
+  });
+
+  // 최신순 정렬
+  await page.click('a.latest');
+
+  // 1초 대기
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  // 포스트 로딩 대기
+  await page.waitForSelector('ul.event_lists.thumbnail_mode li');
+
+  // 포스트 타이틀 가져오기, ::before 요소가 없는 타이틀만 가져옴
+  const titles = await page.evaluate(() => {
+    return Array.from(
+      document.querySelectorAll('ul.event_lists.thumbnail_mode li h5.title')
+    )
+      .filter((titleElement) => {
+        // ::before 스타일이 있는지 확인
+        const beforeContent = window.getComputedStyle(
+          titleElement,
+          '::before'
+        ).content;
+        return (
+          !beforeContent || beforeContent === 'none' || beforeContent === '""'
+        );
+      })
+      .map(
+        (titleElement) =>
+          `${titleElement.innerText.trim()}\nhttps://onoffmix.com/${titleElement
+            .closest('a')
+            ?.getAttribute('href')}\n\n`
+      );
+  });
+
+  // 새로운 포스트 타이틀 필터링
+  const newPostTitles = titles.filter(
+    (title) => !lastPostTitles.includes(title.split('\n')[0])
+  );
+
+  // 기존 타이틀 중 새로운 데이터에 없는 항목 삭제
+  const titlesToDelete = lastPostTitles.filter(
+    (title) => !titles.map((t) => t.split('\n')[0]).includes(title)
+  );
+
+  return { newPostTitles, titlesToDelete };
+}
+
+module.exports = onOffMixScrape;
--- services/cron/getOnOffMixPosts.js
+++ services/cron/getOnOffMixPosts.js
@@ -2,10 +2,9 @@ const Post = require('../../models/Post');
 
 const puppeteer = require('puppeteer-core');
 const chromium = require('@sparticuz/chromium');
+const onOffMixScrape = require('../../scrapes/onOffMix');
 
 let lastPostTitles = []; // 마지막으로 확인한 포스트의 제목
-
-let newPostTitles = []; // 새로운 포스트의 제목
 
 // DB에서 latest 컬렉션의 모든 타이틀을 배열로 가져오는 함수
 async function loadLastPostTitles() {
@@ -19,80 +18,11 @@ async function getOnOffMixPosts() {
 
   console.log('lastPostTitles:', lastPostTitles);
 
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath(),
-    headless: chromium.headless,
-    ignoreHTTPSErrors: true,
-  });
+  const { newPostTitles, titlesToDelete } = await onOffMixScrape(
+    lastPostTitles
+  ); // OnOffMix 스크래핑 후 새로운 포스트와 삭제할 포스트의 제목을 가져옴
 
-  const page = await browser.newPage(); // 새 페이지 생성
-
-  await page.goto('https://onoffmix.com/event/main/?c=105'); // 페이지 URL
-
-  // 로딩 대기
-  await page.waitForSelector('fieldset.filter_category_area');
-
-  // 주제 드롭다운 열기
-  await page.click('fieldset.filter_category_area');
-
-  // 과학/IT/AI 분야 선택
-  await page.evaluate(() => {
-    document
-      .querySelector('fieldset.filter_category_area li:nth-of-type(3) input')
-      .click();
-  });
-
-  // 적용 버튼 클릭
-  await page.evaluate(() => {
-    document
-      .querySelector('fieldset.filter_category_area div.button_area button')
-      .click();
-  });
-
-  // 최신순 정렬
-  await page.click('a.latest');
-
-  // 1초 대기
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  // 포스트 로딩 대기
-  await page.waitForSelector('ul.event_lists.thumbnail_mode li');
-
-  // 포스트 타이틀 가져오기, ::before 요소가 없는 타이틀만 가져옴
-  const titles = await page.evaluate(() => {
-    return Array.from(
-      document.querySelectorAll('ul.event_lists.thumbnail_mode li h5.title')
-    )
-      .filter((titleElement) => {
-        // ::before 스타일이 있는지 확인
-        const beforeContent = window.getComputedStyle(
-          titleElement,
-          '::before'
-        ).content;
-        return (
-          !beforeContent || beforeContent === 'none' || beforeContent === '""'
-        );
-      })
-      .map(
-        (titleElement) =>
-          `${titleElement.innerText.trim()}\nhttps://onoffmix.com/${titleElement
-            .closest('a')
-            ?.getAttribute('href')}\n\n`
-      );
-  });
-
-  // 새로운 포스트 타이틀 필터링
-  newPostTitles = titles.filter(
-    (title) => !lastPostTitles.includes(title.split('\n')[0])
-  );
-
-  // 기존 타이틀 중 새로운 데이터에 없는 항목 삭제
-  const titlesToDelete = lastPostTitles.filter(
-    (title) => !titles.map((t) => t.split('\n')[0]).includes(title)
-  );
-
+  // 삭제할 포스트가 있으면 DB에서 삭제
   if (titlesToDelete.length > 0) {
     await Post.deleteMany({ title: { $in: titlesToDelete } });
     console.log('삭제된 포스트 타이틀:', titlesToDelete);
@@ -100,7 +30,7 @@ async function getOnOffMixPosts() {
 
   await browser.close();
 
-  // 새로운 타이틀만 DB에 저장
+  // 새로운 포스트만 DB에 저장
   if (newPostTitles.length > 0) {
     for (const title of newPostTitles) {
       const joinedTitle = title.split('\n')[0];
