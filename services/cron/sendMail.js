ea09bd26cdbeaa9e2e28ff53ebbd353742ffbca0
nagosu <nozick1021@gmail.com>
2024-11-10 23:49:55 +0900

    메일 css 오류 수정

--- services/cron/sendMail.js
+++ services/cron/sendMail.js
@@ -25,17 +25,14 @@ async function sendMail(newOnOffMixPosts, newLinkareerPosts) {
     bcc: emailsToSend, // 숨은 참조
     subject: '새로운 포스트 알림',
     html: `
-      <body style="background-color: #ffffff; color: #000000;">
-        <img src="../../assets/png/logo.png" alt="nagosu" style="width: auto; height: 40px;">
-        <br>
-        <p style="font-size: 26px; font-weight: bold;"><strong>새로운 포스트 알림</strong></p>
-        <br>
-        <p style="font-size: 20px; font-weight: bold;"><strong>OnOffMix</strong></p>
-        <p>${newOnOffMixPosts.split('\n').join('<br>')}</p>
-        <br>
-        <p style="font-size: 20px; font-weight: bold;"><strong>Linkareer</strong></p>
-        <p>${newLinkareerPosts.split('\n').join('<br>')}</p>
-      </body>
+      <img src="https://i.ibb.co/mbd2731/logo.png" alt="nagosu" style="width: auto; height: 40px;">
+      <br>
+      <p style="font-size: 26px; font-weight: bold;"><strong>새로운 포스트 알림</strong></p>
+      <p style="font-size: 20px; font-weight: bold;"><strong>OnOffMix</strong></p>
+      <p>${newOnOffMixPosts.split('\n').join('<br>')}</p>
+      <br>
+      <p style="font-size: 20px; font-weight: bold;"><strong>Linkareer</strong></p>
+      <p>${newLinkareerPosts.split('\n').join('<br>')}</p>
   `,
   };
 
